d9689151b1247c441aadedcd575979490d5a030b
Emil Kowalski <e@emilkowal.ski>
2023-03-04 10:23:44 +0100

    Add fathom analytics

--- website/app/page.tsx
+++ website/app/page.tsx
@@ -11,12 +11,34 @@ import { Footer } from '@/src/components/Footer';
 import { Position } from '@/src/components/Position';
 import { Usage } from '@/src/components/Usage';
 import { Other } from '@/src/components/Other/Other';
+import { useRouter } from 'next/router';
+import * as Fathom from 'fathom-client';
 
 export default function Home() {
   const [expand, setExpand] = React.useState(false);
   const [position, setPosition] = React.useState<Position>('bottom-right');
   const [richColors, setRichColors] = React.useState(false);
   const [closeButton, setCloseButton] = React.useState(false);
+
+  const router = useRouter();
+
+  React.useEffect(() => {
+    if (process.env.NODE_ENV === 'production') {
+      Fathom.load(process.env.NEXT_PUBLIC_FATHOM_SITE_ID as string, {
+        includedDomains: ['sonner.emilkowal.ski'],
+      });
+    }
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview();
+    }
+
+    router.events.on('routeChangeComplete', onRouteChangeComplete);
+
+    return () => {
+      router.events.off('routeChangeComplete', onRouteChangeComplete);
+    };
+  }, [router.events]);
 
   return (
     <>
