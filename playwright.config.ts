a880fde6ad4c67f20225614d6a9af33dd0695416
emilkowalski <e@emilkowal.ski>
2023-03-11 00:15:35 +0100

    Fix tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,7 +10,7 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './tests',
+  testDir: './test',
   /* Maximum time one test can run for. */
   timeout: 30 * 1000,
   expect: {
