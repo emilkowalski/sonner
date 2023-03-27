e7c4f571e43d5d641a435c3d764df4a7a5bfcc8b
Jay Welsh <thealchemicalopus@gmail.com>
2023-03-27 09:15:29 +0200

    fix(styles/assets): color adjustments & icon adjustment

--- src/assets.tsx
+++ src/assets.tsx
@@ -43,11 +43,11 @@ const SuccessIcon = (
 );
 
 const WarningIcon = (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" height="16" width="20">
-    <path
-      fillRule="evenodd"
-      d="M1 21h22L12 2 1 21zm12-3h-2v-2h2v2zm0-4h-2v-4h2v4z"
-      clipRule="evenodd"
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" height="20" width="20">
+    <path 
+      fill-rule="evenodd" 
+      d="M9.401 3.003c1.155-2 4.043-2 5.197 0l7.355 12.748c1.154 2-.29 4.5-2.599 4.5H4.645c-2.309 0-3.752-2.5-2.598-4.5L9.4 3.003zM12 8.25a.75.75 0 01.75.75v3.75a.75.75 0 01-1.5 0V9a.75.75 0 01.75-.75zm0 8.25a.75.75 0 100-1.5.75.75 0 000 1.5z" 
+      clip-rule="evenodd"
     />
   </svg>
 );
