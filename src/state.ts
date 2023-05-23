29544c282aedb9bb71ffba36eed9e29b830370ab
emilkowalski <e@emilkowal.ski>
2023-05-22 21:00:39 -0700

    Fix logic

--- src/state.ts
+++ src/state.ts
@@ -29,7 +29,7 @@ class Observer {
 
   create = (data: ExternalToast & { message?: string | React.ReactNode; type?: ToastTypes }) => {
     const { message, ...rest } = data;
-    const id = typeof data?.id === 'number' || data?.id.length > 0 ? data.id : toastsCounter++;
+    const id = typeof data?.id === 'number' || data.id?.length > 0 ? data.id : toastsCounter++;
     const alreadyExists = this.toasts.find((toast) => {
       return toast.id === id;
     });
