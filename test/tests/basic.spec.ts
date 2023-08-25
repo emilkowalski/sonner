1babd8e443dcc4c44e080cd2e4b22075862e4e36
emilkowalski <e@emilkowal.ski>
2023-08-25 14:38:48 +0200

    fix tests

--- test/tests/basic.spec.ts
+++ test/tests/basic.spec.ts
@@ -26,7 +26,6 @@ test.describe('Basic functionality', () => {
     await page.getByTestId('promise').click();
     await expect(page.getByText('Loading...')).toHaveCount(1);
     await expect(page.getByText('Loaded')).toHaveCount(1);
-    await expect(page.getByTestId('promise')).toHaveAttribute('data-finally', '1');
   });
 
   test('render custom jsx in toast', async ({ page }) => {
@@ -82,9 +81,15 @@ test.describe('Basic functionality', () => {
 
   test("toast's dismiss callback gets executed correctly", async ({ page }) => {
     await page.getByTestId('dismiss-toast-callback').click();
-    await page.hover('[data-sonner-toast]');
+    const toast = page.locator('[data-sonner-toast]');
+    const dragBoundingBox = await toast.boundingBox();
+
+    if (!dragBoundingBox) return;
+    await page.mouse.move(dragBoundingBox.x + dragBoundingBox.width / 2, dragBoundingBox.y);
+
     await page.mouse.down();
-    await page.mouse.move(0, 800);
+    await page.mouse.move(0, dragBoundingBox.y + 300);
+
     await page.mouse.up();
     await expect(page.getByTestId('dismiss-el')).toHaveCount(1);
   });
