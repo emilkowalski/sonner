b93ba18b23c82af49742bc9dbbcef7776331ecd1
Stephen Zhou <hi@hyoban.cc>
2023-09-18 21:10:47 +0800

    feat: allow setting position when creating a toast

--- src/types.ts
+++ src/types.ts
@@ -37,6 +37,7 @@ export interface ToastT {
   style?: React.CSSProperties;
   className?: string;
   descriptionClassName?: string;
+  position?: Position;
 }
 
 export type Position = 'top-left' | 'top-right' | 'bottom-left' | 'bottom-right' | 'top-center' | 'bottom-center';
