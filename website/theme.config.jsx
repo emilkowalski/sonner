1925e03d0329fcad21c0ef86af95b0ba13540d77
emilkowalski <e@emilkowal.ski>
2023-10-04 02:19:51 +0200

    Update docs repo base url

--- website/theme.config.jsx
+++ website/theme.config.jsx
@@ -3,7 +3,7 @@ export default {
   project: {
     link: 'https://github.com/emilkowalski/sonner',
   },
-  docsRepositoryBase: 'https://github.com/emilkowalski/sonner',
+  docsRepositoryBase: 'https://github.com/emilkowalski/sonner/tree/main',
   useNextSeoProps() {
     return {
       titleTemplate: '%s – Sonner',
