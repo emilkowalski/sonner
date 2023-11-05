667d18469aba8048e01efb6aa72b10f70bf0fe40
emilkowalski <e@emilkowal.ski>
2023-11-05 13:50:14 +0100

    Close custom toasts

--- website/src/components/Hero/index.tsx
+++ website/src/components/Hero/index.tsx
@@ -3,6 +3,21 @@ import { toast } from 'sonner';
 import styles from './hero.module.css';
 import Link from 'next/link';
 
+export const headlessToast = (message: string) => {
+  toast.custom(
+    (t) => (
+      <div>
+        {message}
+        <button onClick={() => toast.dismiss(t)}>x</button>
+      </div>
+    ),
+    {
+      // dismissible: true
+      // onDismiss: (t) => toast.dismiss(t.id)
+    },
+  );
+};
+
 export const Hero = () => {
   return (
     <div className={styles.wrapper}>
@@ -17,6 +32,7 @@ export const Hero = () => {
         <button
           data-primary=""
           onClick={() => {
+            headlessToast('headless toast');
             toast('Sonner', {
               description: 'An opinionated toast component for React.',
             });
@@ -25,6 +41,7 @@ export const Hero = () => {
         >
           Render a toast
         </button>
+        <button onClick={() => toast.dismiss()}>dismiss all</button>
         <a className={styles.button} href="https://github.com/emilkowalski/sonner" target="_blank">
           GitHub
         </a>
