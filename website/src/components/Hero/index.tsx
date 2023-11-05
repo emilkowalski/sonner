45154bcc7b0eb3feb594cba92e9e5f45036768a7
emilkowalski <e@emilkowal.ski>
2023-11-05 13:51:00 +0100

    Remove hero updates

--- website/src/components/Hero/index.tsx
+++ website/src/components/Hero/index.tsx
@@ -3,21 +3,6 @@ import { toast } from 'sonner';
 import styles from './hero.module.css';
 import Link from 'next/link';
 
-export const headlessToast = (message: string) => {
-  toast.custom(
-    (t) => (
-      <div>
-        {message}
-        <button onClick={() => toast.dismiss(t)}>x</button>
-      </div>
-    ),
-    {
-      // dismissible: true
-      // onDismiss: (t) => toast.dismiss(t.id)
-    },
-  );
-};
-
 export const Hero = () => {
   return (
     <div className={styles.wrapper}>
@@ -32,7 +17,6 @@ export const Hero = () => {
         <button
           data-primary=""
           onClick={() => {
-            headlessToast('headless toast');
             toast('Sonner', {
               description: 'An opinionated toast component for React.',
             });
@@ -41,7 +25,6 @@ export const Hero = () => {
         >
           Render a toast
         </button>
-        <button onClick={() => toast.dismiss()}>dismiss all</button>
         <a className={styles.button} href="https://github.com/emilkowalski/sonner" target="_blank">
           GitHub
         </a>
