6902792ee5c429e4ca3b26f959fa2ac2fdf42550
emilkowalski <e@emilkowal.ski>
2023-12-25 23:59:20 +0100

    Remove toast classes from custom loadingIcon

--- src/index.tsx
+++ src/index.tsx
@@ -196,10 +196,7 @@ const Toast = (props: ToastProps) => {
   function getLoadingIcon() {
     if (loadingIconProp) {
       return (
-        <div
-          className={cn('loader', classNames?.toast, toast?.classNames?.toast)}
-          data-visible={toastType === 'loading'}
-        >
+        <div className="loader" data-visible={toastType === 'loading'}>
           {loadingIconProp}
         </div>
       );
@@ -398,7 +395,7 @@ const Toast = (props: ToastProps) => {
 
 function getDocumentDirection(): ToasterProps['dir'] {
   if (typeof window === 'undefined') return 'ltr';
-  if (typeof document === 'undefined') return 'ltr';  // For Fresh purpose
+  if (typeof document === 'undefined') return 'ltr'; // For Fresh purpose
 
   const dirAttribute = document.documentElement.getAttribute('dir');
 
