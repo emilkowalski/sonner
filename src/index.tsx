35610be3314273682fc3cbf4423bd6b12c95c852
Sam Apostel <sam@apostel.be>
2024-05-23 09:31:41 +0200

    export ToasterProps type (#400)
    For Sonner to be bundled in other libraries, we need to export `ToasterProps`.
    
    This fixes following error for library authors:
    ```
    TS4023: Exported variable 'X' has or is using name 'ToasterProps' from external module './node_modules/sonner/dist/index' but cannot be named
    ```

--- src/index.tsx
+++ src/index.tsx
@@ -722,4 +722,4 @@ const Toaster = (props: ToasterProps) => {
     </section>
   );
 };
-export { toast, Toaster, type ExternalToast, type ToastT };
+export { toast, Toaster, type ExternalToast, type ToastT, type ToasterProps };
