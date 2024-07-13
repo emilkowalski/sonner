7ad018d0bf08bf7c3fa9981c34b5593f20fb1fe7
eliezerbasubi <eliezer.basubi30@gmail.com>
2024-07-13 16:30:41 +0300

    fix: custom loading icon not showing

--- src/assets.tsx
+++ src/assets.tsx
@@ -23,15 +23,23 @@ export const getAsset = (type: ToastTypes): JSX.Element | null => {
 
 const bars = Array(12).fill(0);
 
-export const Loader = ({ visible }: { visible: boolean }) => {
+export const LoadingWrapper = ({ visible, children }: React.PropsWithChildren<{ visible: boolean }>) => {
   return (
     <div className="sonner-loading-wrapper" data-visible={visible}>
+      {children}
+    </div>
+  );
+};
+
+export const Loader = ({ visible }: { visible: boolean }) => {
+  return (
+    <LoadingWrapper visible={visible}>
       <div className="sonner-spinner">
         {bars.map((_, i) => (
           <div className="sonner-loading-bar" key={`spinner-bar-${i}`} />
         ))}
       </div>
-    </div>
+    </LoadingWrapper>
   );
 };
 
