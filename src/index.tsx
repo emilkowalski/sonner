cec5a85790f840d30f3972f17afecb8a18f63b60
알노리 <67145096+ynng3@users.noreply.github.com>
2025-02-16 19:59:01 +0900

    chore: Hide the close button when toastType is loading (#516)
    #514 #476

--- src/index.tsx
+++ src/index.tsx
@@ -410,7 +410,7 @@ const Toast = (props: ToastProps) => {
         toastRef.current?.style.setProperty('--swipe-amount-y', `${swipeAmount.y}px`);
       }}
     >
-      {closeButton && !toast.jsx ? (
+      {closeButton && !toast.jsx && toastType !== 'loading' ? (
         <button
           aria-label={closeButtonAriaLabel}
           data-disabled={disabled}
